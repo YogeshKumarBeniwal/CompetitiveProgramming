8928e210bf2f572866dac11ea8ec4fb6727da9eb
YogeshBen <yogeshkumarbeniwal1999@gmail.com>
2021-12-14 22:57:48 +0530

    Rotate array in O(n)

--- LeetCode/RotateArray.cpp
+++ LeetCode/RotateArray.cpp
@@ -1,26 +1,74 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
+int GetNewIndex(int current, int steps, int size)
+{
+    int s = current + steps;
+    size -= 1;
+
+    return s > size ? s - size - 1 : s;
+}
+
+int GetMinSteps(int steps, int size)
+{
+    float f = (float)steps / (float)size;
+    f = f - (int)f;
+
+    return round(f * size);
+}
+
 vector<int> rotate(vector<int> &nums, int k)
 {
-    int size = nums.size(); 
-    for(int i=0; i<=k; i++)
+    int size = nums.size();
+
+    if (size == 1 || k == 0)
     {
-        int x = nums[size - (k-i)];
-        //nums[i] += nums[size - (k-i)];
-        nums[size - (k-i)] = nums[i];
-        nums[i] = x;
+        return nums;
     }
 
+    int minSteps = k > size ? GetMinSteps(k, size) : k;
+
+    if (minSteps == size)
+    {
+        return nums;
+    }
+
+    int temp = nums[0], ni;
+
+    int l1 = 1, l2 = 1;
+    bool isFirst = true;
+    for (int i = 0; i < l1; i++)
+    {
+        ni = i;
+        temp = nums[i];
+        for (int j = 0; j < l2; j++)
+        {
+            ni = GetNewIndex(ni, minSteps, size);
+            temp = temp + nums[ni];
+            nums[ni] = temp - nums[ni];
+            temp = temp - nums[ni];
+            if (ni == 0)
+            {
+                l1 = size / l2;
+                isFirst = false;
+                break;
+            }
+            else if (isFirst)
+            {
+                l2++;
+            }
+        }
+    }
     return nums;
 }
 
 int main()
 {
-    vector<int> nums{-4, -3, -2, -1, 0};
-    vector<int> sqs = rotate(nums, 4);
+    vector<int> nums{1,2,3,4,5,6};
+    vector<int> sqs = rotate(nums, 1);
 
     for (int i = 0; i < sqs.size(); i++)
     {
